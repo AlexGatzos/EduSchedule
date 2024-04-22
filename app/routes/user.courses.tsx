e5391def490b15e242678f0fb2a6257ef26825c2
AlexGatzos <alexgatzo@hotmail.gr>
2024-04-22 09:20:00 +0200

    user view courses and teachers

---
+++ app/routes/user.courses.tsx
@@ -0,0 +1,94 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node"; // or cloudflare/deno
+
+import { prisma } from "~/database.server";
+import { useLoaderData } from "@remix-run/react";
+import {
+  Heading,
+  Section,
+  Collection,
+  ListBox,
+  ListBoxItem,
+  Header,
+} from "react-aria-components";
+import { authenticator } from "~/services/auth.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  let user = await authenticator.isAuthenticated(request);
+
+  if (!user) {
+    return redirect("/auth/login");
+  }
+
+  if (!user.profile.isAdmin) {
+    return redirect("/");
+  }
+
+  let courses = await prisma.course.findMany();
+
+  let coursesBySemester = courses.reduce(
+    (acc, course) => {
+      if (course.semester in acc) {
+        acc[course.semester].push(course);
+      } else {
+        acc[course.semester] = [course];
+      }
+      return acc;
+    },
+    {} as Record<string, typeof courses>,
+  );
+
+  let allCourses = Object.entries(coursesBySemester).map(
+    ([semester, courses]) => {
+      return {
+        id: semester,
+        semester,
+        courses,
+      };
+    },
+  );
+
+  return json({ allCourses });
+}
+
+export default function Courses() {
+  let { allCourses } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex w-full items-center justify-between gap-2">
+          <Heading className="text-2xl font-bold">Courses</Heading>
+          <div className="flex items-center gap-2"></div>
+        </div>
+        <ListBox className="flex flex-col gap-4" items={allCourses}>
+          {(semesterWithCourses) => (
+            <Section
+              className="flex flex-col divide-y overflow-hidden rounded border"
+              key={semesterWithCourses.id}
+            >
+              <Header className="bg-indigo-50 px-2 py-4 text-lg font-semibold text-indigo-500">
+                Εξάμηνο {semesterWithCourses.semester}
+              </Header>
+              <Collection items={semesterWithCourses.courses}>
+                {(course) => (
+                  <ListBoxItem
+                    className="flex items-center justify-between gap-2 px-2 py-3"
+                    key={course.id}
+                  >
+                    <span className="flex-shrink-0 text-xs text-gray-800">
+                      {course.type}
+                    </span>
+                    <span className="flex-1 truncate text-sm font-medium text-gray-900">
+                      {course.name}
+                    </span>
+                  </ListBoxItem>
+                )}
+              </Collection>
+            </Section>
+          )}
+        </ListBox>
+      </div>
+    </>
+  );
+}
---
+++ app/routes/user.teachers.tsx
@@ -0,0 +1,129 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node"; // or cloudflare/deno
+
+import { prisma } from "~/database.server";
+import { useLoaderData } from "@remix-run/react";
+import {
+  Heading,
+  Section,
+  Collection,
+  ListBox,
+  ListBoxItem,
+  Header,
+} from "react-aria-components";
+import { useState } from "react";
+import { CreateTeacherModal } from "~/components/create-teachers-modal";
+import { ViewTeacherModal } from "~/components/view-teachers-modal";
+import { authenticator } from "~/services/auth.server";
+import { UserIcon } from "@heroicons/react/24/solid";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  let user = await authenticator.isAuthenticated(request);
+
+  if (!user) {
+    return redirect("/auth/login");
+  }
+
+  let teachers = await prisma.teacher.findMany({
+    include: {
+      courses: {
+        select: {
+          course: true,
+        },
+      },
+    },
+  });
+
+  let teacherByName = teachers.reduce(
+    (acc, teacher) => {
+      if (teacher.fullName in acc) {
+        acc[teacher.fullName].push(teacher);
+      } else {
+        acc[teacher.fullName] = [teacher];
+      }
+      return acc;
+    },
+    {} as Record<string, typeof teachers>,
+  );
+
+  let allTeachers = Object.entries(teacherByName).map(
+    ([fullName, teachers]) => {
+      return {
+        id: fullName,
+        fullName,
+        teachers,
+      };
+    },
+  );
+
+  return json({ allTeachers, teachers });
+}
+
+export default function Teacher() {
+  let { allTeachers, teachers } = useLoaderData<typeof loader>();
+
+  let [isNewTeacherModalOpen, setIsNewTeacherModalOpen] = useState(false);
+  let [teacherId, setTeacherId] = useState<number | null>(null);
+
+  let selectedTeacher = teacherId
+    ? teachers.find((c) => c.id === teacherId)
+    : null;
+
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex w-full items-center justify-between gap-2">
+          <Heading className="text-2xl font-bold">Teachers</Heading>
+          <div className="flex items-center gap-2"></div>
+        </div>
+        <ListBox className="flex flex-col gap-4" items={allTeachers}>
+          {(teacher) => (
+            <Section
+              className="flex flex-col divide-y overflow-hidden rounded border"
+              key={teacher.id}
+            >
+              <Header className="flex items-center gap-2 bg-indigo-50 px-2 py-3 text-lg font-semibold text-indigo-700">
+                <UserIcon className="w-5" /> {teacher.fullName}
+              </Header>
+
+              <Collection items={teacher.teachers}>
+                {(teacher) => (
+                  <ListBoxItem
+                    className="flex items-center justify-between"
+                    key={teacher.id}
+                  >
+                    <div className="flex w-full flex-col divide-y">
+                      {teacher.courses.map((course) => (
+                        <div className="px-8 py-2" key={course.course.name}>
+                          <span>{course.course.name}</span>
+                          {/* Render other course details as needed */}
+                        </div>
+                      ))}
+                    </div>
+                  </ListBoxItem>
+                )}
+              </Collection>
+            </Section>
+          )}
+        </ListBox>
+      </div>
+
+      <CreateTeacherModal
+        isOpen={isNewTeacherModalOpen}
+        onOpenChange={setIsNewTeacherModalOpen}
+      />
+
+      {selectedTeacher && (
+        <ViewTeacherModal
+          isOpen={teacherId !== null}
+          onOpenChange={(isOpen) => {
+            if (!isOpen) {
+              setTeacherId(null);
+            }
+          }}
+          teacher={selectedTeacher}
+        />
+      )}
+    </>
+  );
+}
